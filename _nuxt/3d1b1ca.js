b21970ba18c2076f94465d638a3cf3b90ddfcf80
Leo36094 <Leo36094@users.noreply.github.com>
2023-08-14 14:48:20 +0000

    deploy: e6d300695fc8d73e07f4471ce52714e884c8bef3

---
+++ _nuxt/3d1b1ca.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{336:function(n,t,e){"use strict";e.r(t);var l=e(38),component=Object(l.a)({},(function(){return(0,this._self._c)("div",[this._v("Lottery")])}),[],!1,null,null,null);t.default=component.exports}}]);
