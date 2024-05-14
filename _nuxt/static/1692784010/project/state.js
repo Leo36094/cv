ca0062c45189e446159e7fa9719153cb81e8bfee
Leo36094 <Leo36094@users.noreply.github.com>
2024-05-14 01:29:23 +0000

    deploy: 3fdbe612a4e5763b7cdcab5a4550ddb40c414670

--- _nuxt/static/1692784010/project/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcv\u002F_nuxt\u002Fstatic\u002F1692784010",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject",config:{_app:{basePath:"\u002Fcv\u002F",assetsPath:"\u002Fcv\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1715650162/project/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcv\u002F_nuxt\u002Fstatic\u002F1715650162",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject",config:{_app:{basePath:"\u002Fcv\u002F",assetsPath:"\u002Fcv\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
